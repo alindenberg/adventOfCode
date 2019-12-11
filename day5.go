bc984590cc12a179e0994cf18e8b6181466e5716
Austin Lindenberg <alindenberg8@gmail.com>
2019-12-11 11:48:30 -0500

    Day 5

---
+++ day5.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	input := []int{3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 27, 28, 225, 1, 113, 14, 224, 1001, 224, -34, 224, 4, 224, 102, 8, 223, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1102, 52, 34, 224, 101, -1768, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 6, 224, 224, 1, 223, 224, 223, 1002, 187, 14, 224, 1001, 224, -126, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 224, 223, 223, 1102, 54, 74, 225, 1101, 75, 66, 225, 101, 20, 161, 224, 101, -54, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 7, 224, 1, 224, 223, 223, 1101, 6, 30, 225, 2, 88, 84, 224, 101, -4884, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 2, 224, 224, 1, 224, 223, 223, 1001, 214, 55, 224, 1001, 224, -89, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 224, 223, 223, 1101, 34, 69, 225, 1101, 45, 67, 224, 101, -112, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1102, 9, 81, 225, 102, 81, 218, 224, 101, -7290, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 5, 224, 224, 1, 223, 224, 223, 1101, 84, 34, 225, 1102, 94, 90, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 1007, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 329, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 344, 101, 1, 223, 223, 1008, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 359, 101, 1, 223, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 374, 101, 1, 223, 223, 108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 404, 1001, 223, 1, 223, 7, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 419, 101, 1, 223, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 434, 1001, 223, 1, 223, 1107, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 449, 101, 1, 223, 223, 1108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 464, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 479, 101, 1, 223, 223, 8, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 494, 1001, 223, 1, 223, 1007, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 509, 1001, 223, 1, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 524, 1001, 223, 1, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 539, 101, 1, 223, 223, 1008, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 554, 101, 1, 223, 223, 107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 569, 101, 1, 223, 223, 107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 584, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 599, 101, 1, 223, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 614, 1001, 223, 1, 223, 107, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 629, 101, 1, 223, 223, 7, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 644, 1001, 223, 1, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 659, 101, 1, 223, 223, 108, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226}
+	getIntcode(input)
+}
+
+func getIntcode(input []int) []int {
+	i := 0
+	for i < len(input) {
+		instruction := input[i]
+
+		opcode := instruction % 100
+		instruction /= 100
+
+		if opcode == 99 {
+			return input
+		}
+
+		switch opcode {
+		case 1:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+			instruction /= 10
+
+			index2 := input[i+2]
+			mode2 := instruction % 10
+			val2 := index2
+			if mode2 == 0 {
+				val2 = input[index2]
+			}
+			instruction /= 10
+
+			targetIndex := input[i+3]
+
+			sum := val1 + val2
+			input[targetIndex] = sum
+
+			i += 4
+			break
+		case 2:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+			instruction /= 10
+
+			index2 := input[i+2]
+			mode2 := instruction % 10
+			val2 := index2
+			if mode2 == 0 {
+				val2 = input[index2]
+			}
+			instruction /= 10
+
+			targetIndex := input[i+3]
+
+			product := val1 * val2
+			input[targetIndex] = product
+
+			i += 4
+			break
+		case 3:
+			fmt.Print("Input: ")
+
+			var userInput int
+			_, err := fmt.Scanf("%d", &userInput)
+			if err != nil {
+				log.Fatal("Error on user input.", err.Error())
+			}
+
+			targetIndex := input[i+1]
+			input[targetIndex] = userInput
+
+			i += 2
+			break
+		case 4:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			val := index1
+			if mode1 == 0 {
+				val = input[index1]
+			}
+
+			i += 2
+			break
+		case 5:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			instruction /= 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+
+			if val1 != 0 {
+				index2 := input[i+2]
+				mode2 := instruction % 10
+				newInstructionPointer := index2
+				if mode2 == 0 {
+					newInstructionPointer = input[index2]
+				}
+				i = newInstructionPointer
+			} else {
+				i += 3
+			}
+			break
+		case 6:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			instruction /= 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+
+			if val1 == 0 {
+				index2 := input[i+2]
+				mode2 := instruction % 10
+				newInstructionPointer := index2
+				if mode2 == 0 {
+					newInstructionPointer = input[index2]
+				}
+				i = newInstructionPointer
+			} else {
+				i += 3
+			}
+			break
+		case 7:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+			instruction /= 10
+
+			index2 := input[i+2]
+			mode2 := instruction % 10
+			val2 := index2
+			if mode2 == 0 {
+				val2 = input[index2]
+			}
+			instruction /= 10
+
+			targetIndex := input[i+3]
+
+			if val1 < val2 {
+				input[targetIndex] = 1
+			} else {
+				input[targetIndex] = 0
+			}
+
+			i += 4
+		case 8:
+			index1 := input[i+1]
+			mode1 := instruction % 10
+			val1 := index1
+			if mode1 == 0 {
+				val1 = input[index1]
+			}
+			instruction /= 10
+
+			index2 := input[i+2]
+			mode2 := instruction % 10
+			val2 := index2
+			if mode2 == 0 {
+				val2 = input[index2]
+			}
+			instruction /= 10
+
+			targetIndex := input[i+3]
+
+			if val1 == val2 {
+				input[targetIndex] = 1
+			} else {
+				input[targetIndex] = 0
+			}
+
+			i += 4
+		default:
+			fmt.Println("Error. Got opcode ", opcode, "on instruction ", i)
+			return input
+		}
+	}
+	return input
+}
